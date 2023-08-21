ed25b96f06b450b6da28a3306f373c946735842e
Vinh Tran <42789132+kimvinh@users.noreply.github.com>
2023-08-21 14:12:08 -0700

    Update

--- src/localNetwork.js
+++ src/localNetwork.js
@@ -1,3 +1,3 @@
-// export const localNetwork = 'https://chatapp-api-kkfv.onrender.com';
+export const localNetwork = 'https://chatapp-api-kkfv.onrender.com';
 
-export const localNetwork = 'http://192.168.1.211:3001';
+// export const localNetwork = 'http://192.168.1.211:3001';
